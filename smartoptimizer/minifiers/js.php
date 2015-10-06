3f0ee13fe67a2c005d6d12cdbddfd41fbe1468c6
SixK <dasixk@free.fr>
2015-10-06 10:42:47 +0200

    Update js.php
    Avoid to evaluate strlen($str) each time.
    Few milliseconds won.

--- smartoptimizer/minifiers/js.php
+++ smartoptimizer/minifiers/js.php
@@ -3,11 +3,12 @@
  * SmartOptimizer JavaScript Minifier
  */
 function minify_js($str) {
+	$strsize=strlen($str);
 	$res = '';
 	$maybe_regex = true;
 	$i=0;
 	$current_char = '';
-	while ($i+1<strlen($str)) {
+	while ($i+1<$strsize) {
 		if ($maybe_regex && $str[$i]==='/' && $str[$i+1]!=='/' && $str[$i+1]!=='*' && @$str[$i-1]!=='*') {//regex detected
 			if (strlen($res) && $res[strlen($res)-1] === '/') $res .= ' ';
 			do {
@@ -19,10 +20,10 @@ function minify_js($str) {
 							$res .= $str[$i++];
 						}
 						$res .= $str[$i++];
-					} while ($i<strlen($str) && $str[$i]!==']');
+					} while ($i<$strsize && $str[$i]!==']');
 				}
 				$res .= $str[$i++];
-			} while ($i<strlen($str) && $str[$i]!=='/');
+			} while ($i<$strsize && $str[$i]!=='/');
 			$res .= $str[$i++];
 			$maybe_regex = false;
 			continue;
@@ -33,17 +34,17 @@ function minify_js($str) {
 					$res .= $str[$i++];
 				}
 				$res .= $str[$i++];
-			} while ($i<strlen($str) && $str[$i]!==$quote);
+			} while ($i<$strsize && $str[$i]!==$quote);
 			$res .= $str[$i++];
 			continue;
 		} elseif ($str[$i].$str[$i+1]==='/*' && @$str[$i+2]!=='@') {//multi-line comment detected
 			$i+=3;
-			while ($i<strlen($str) && $str[$i-1].$str[$i]!=='*/') $i++;
+			while ($i<$strsize && $str[$i-1].$str[$i]!=='*/') $i++;
 			if ($current_char == "\n") $str[$i] = "\n";
 			else $str[$i] = ' ';
 		} elseif ($str[$i].$str[$i+1]==='//') {//single-line comment detected
 			$i+=2;
-			while ($i<strlen($str) && $str[$i]!=="\n" && $str[$i]!=="\r") $i++;
+			while ($i<$strsize && $str[$i]!=="\n" && $str[$i]!=="\r") $i++;
 		}
 		
 
@@ -54,13 +55,13 @@ function minify_js($str) {
 				if ($res[strlen($res)-1] === "\n") $LF_needed = true;
 				$res = substr($res, 0, -1);
 			}
-			while ($i+1<strlen($str) && preg_match('/[\n\r\t ]/', $str[$i+1])) {
+			while ($i+1<$strsize && preg_match('/[\n\r\t ]/', $str[$i+1])) {
 				if (!$LF_needed && preg_match('/[\n\r]/', $str[$i])) $LF_needed = true;
 				$i++;
 			}
 		}
 		
-		if (strlen($str) <= $i+1) break;
+		if ($strsize <= $i+1) break;
 		
 		$current_char = $str[$i];
 		
@@ -80,7 +81,7 @@ function minify_js($str) {
 				(
 				preg_match('/^[^({[=+\-*%&|!><?:~^,;\/][^)}\]=+\-*%&|><?:,;\/]$/', $res[strlen($res)-1].$str[$i+1]) ||
 				(strlen($res)>1 && preg_match('/^(\+\+)|(--)$/', $res[strlen($res)-2].$res[strlen($res)-1])) ||
-				(strlen($str)>$i+2 && preg_match('/^(\+\+)|(--)$/', $str[$i+1].$str[$i+2])) ||
+				($strsize>$i+2 && preg_match('/^(\+\+)|(--)$/', $str[$i+1].$str[$i+2])) ||
 				preg_match('/^(\+\+)|(--)$/', $res[strlen($res)-1].$str[$i+1])// || // for example i+ ++j;
 				)) $res .= $current_char;
 		} else $res .= $current_char;
@@ -91,7 +92,7 @@ function minify_js($str) {
 		
 		$i++;
 	}
-	if ($i<strlen($str) && preg_match('/[^\n\r\t ]/', $str[$i])) $res .= $str[$i];
+	if ($i<$strsize && preg_match('/[^\n\r\t ]/', $str[$i])) $res .= $str[$i];
 	return $res;
 }
 ?>
